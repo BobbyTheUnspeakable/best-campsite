2de8adc8b10a198864b3246017cffa9804e19a82
Adam Karr <adamlkarr@gmail.com>
2018-08-01 22:56:15 -0500

    Initial commit 2 "Let's try this again from scratch"

---
+++ src/main/java/com/bestcampsite/bestcampsite/BestcampsiteApplication.java
@@ -0,0 +1,12 @@
+package com.bestcampsite.bestcampsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestcampsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestcampsiteApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bestcampsite/bestcampsite/BestcampsiteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bestcampsite.bestcampsite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BestcampsiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
