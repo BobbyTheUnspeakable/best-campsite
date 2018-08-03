3c4b3726c81cf0b8f72214471fc1fed8c05ae636
Adam Karr <adamlkarr@gmail.com>
2018-08-03 12:11:14 -0500

    Added barebones verification to CreateAccount

--- src/main/java/com/bestcampsite/bestcampsite/controllers/UserController.java
+++ src/main/java/com/bestcampsite/bestcampsite/controllers/UserController.java
@@ -3,7 +3,9 @@ package com.bestcampsite.bestcampsite.controllers;
 import com.bestcampsite.bestcampsite.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("bestcampsite")
@@ -13,5 +15,16 @@ public class UserController {
     public String createAccount(Model model){
        model.addAttribute(new User());
        return "User/CreateAccount";
+    }
+
+    @RequestMapping(value = "createAccount", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute User user, String verify){
+        if(user.getPassword().equals(verify)){
+            return "User/Login"; //TODO Change this to the Search Page once its built
+        } else {
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("error", "Passwords do not match");
+            return "User/CreateAccount";
+        }
     }
 }
